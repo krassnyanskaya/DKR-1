fa011411b39bd8bc2bf8cdb79e1286ceef0e9d0c
krassnyanskaya <118731628+krassnyanskaya@users.noreply.github.com>
2022-12-16 16:30:22 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,20 @@
+﻿begin
+  var x, y: real;
+  var a := Readinteger('Введите a');
+  var b := Readinteger('Введите b');
+  var c := Readinteger('Введите c');
+  x := a;
+  while x <= b do
+  begin
+    if x < -10 then
+      y := cos(x) / sin(x) - cos(2 * x) / x * x * x;
+    if (-10 <= x) and (x < -5) then
+      y := power(x, 1 / 3 * x) * x; 
+    if (-5 <= x) and (x < 4) then
+      y := tan(x);
+    if 4 <= x then
+      y := cos(x) * cos(2 * x) - cos(2 * x);
+    println('y в точке ', x:2:2, ' равно ', y:2:2);
+    x := x + c;
+  end;
+end.
